f3fd13111ee78158b58604c592a4bb9480fc9f6b
Facundo Masino <facujmasino@gmail.com>
2024-09-15 20:47:38 -0300

    Update OG Image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Sitio Web Iglesia Filadelfia - Laguna de Lobos",
     images:
-      "https://ik.imagekit.io/facumasino/filadelfialaguna.com.ar/og_filadelfialaguna.png?updatedAt=1726441691630",
+      "https://ik.imagekit.io/facumasino/filadelfialaguna.com.ar/og_filadelfialaguna.png",
     siteName: "Iglesia Filadelfia - Laguna de Lobos",
     description:
       "Somos una iglesia, perteneciente a la Unión de las Asambleas de Dios. Nuestra misión es alcanzar la palabra de Dios a cada persona que lo necesite, ya que es la que tiene el poder de transformar las vidas.",
